0b3ebbc877a9f67314a83bd959d3c8258fe51357
huchunguang <huchunguang123@gmail.com>
2016-10-27 18:40:29 +0800

    add css f sections test ile

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+
+var_dump( json_decode('{"error_code":40000,"error_msg":"\u9519\u8bef\u7684\u8bf7\u6c42\u5730\u5740\u6216\u65b9\u6cd5"}'),true);
